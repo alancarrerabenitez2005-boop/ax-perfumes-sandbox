fdfb1832a762a84e40f3bf0d803e943952d0be86
alancarrerabenitez2005-boop <alancarrera2005@gmail.com>
2026-09-03 03:39:48 -0300

    Actualizar vistas con login funcional

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegistroUsuarioForm
 
 def registro(request):
@@ -11,6 +13,17 @@ def registro(request):
     else:
         form = RegistroUsuarioForm()
     return render(request, 'usuarios/registro.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'usuarios/login.html', {'form': form})
 
 @login_required
 def home(request):
